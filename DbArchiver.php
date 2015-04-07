929fd97a0b5154d1277ecd5104a2449dac977b93
Aleksandr Petrov <burnb83@gmail.com>
2015-04-07 11:51:53 +0400

    Initial commit

---
+++ DbArchiver.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @copyright (c) 2015, Galament
+ * @author Petrov Aleksanr <burnb83@gmail.com> 
+ * Date: 07.04.2015
+ * Time: 4:13
+ */
+
+namespace burn\dbArchiver;
+
+use yii\base\Model;
+use yii\db\ActiveRecord;
+
+class DbArchiver extends Model
+{
+    private $arcClass;
+
+    /**
+     * @param $arcClass ActiveRecord
+     */
+    public function _construct($arcClass)
+    {
+        $this->arcClass = $arcClass;
+    }
+
+    public function actionStart()
+    {
+        $this->archive($this->arcClass);
+    }
+
+    /**
+     * @param $arcClass ActiveRecord
+     */
+    private function archive($arcClass)
+    {
+        $arcClass::getDb()
+            ->createCommand("SELECT *
+                INTO OUTFILE 'dump.sql'
+                FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"'
+                LINES TERMINATED BY '\n'
+                FROM log_error
+                WHERE log_time < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 1 MONTH))")
+            ->execute();
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+Yii2 DB Archiver.
+===================
+Archive old data from db to file by Yii2 ActiveRecord models. 
+For big tables allow direct file export from mysql database. 
+
+Usage:
+------
+```
+  
+```
+
+
